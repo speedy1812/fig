a1b76346701fe70babc7b8b8da21aa5e8e78fd01
Nathan Day <adayinukraine@gmail.com>
2016-09-16 20:03:17 +0300

    Add initial content and locations page.

--- config.rb
+++ config.rb
@@ -1,3 +1,10 @@
+configure :development do
+  activate :livereload
+end
+
+# Gets ride of the .html extension on URLs
+activate :directory_indexes
+
 set :css_dir, 'stylesheets'
 set :js_dir, 'javascripts'
 set :images_dir, 'images'
